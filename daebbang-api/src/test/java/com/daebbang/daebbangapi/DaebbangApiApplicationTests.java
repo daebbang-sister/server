2f1d587215b8b75cab2d61ef5a4bfa2b176720f2
DooYoungHo <pimang223@naver.com>
2026-02-08 22:12:09 +0900

    ci : PR시 test 돌게 작성
    PR 등록시 test 돌고 실패 시 PR 거부

--- daebbang-api/src/test/java/com/daebbang/daebbangapi/DaebbangApiApplicationTests.java
+++ daebbang-api/src/test/java/com/daebbang/daebbangapi/DaebbangApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.daebbang.daebbangapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DaebbangApiApplicationTests {
 
     @Test
