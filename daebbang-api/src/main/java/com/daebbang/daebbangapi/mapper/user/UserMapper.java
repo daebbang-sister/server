f14e869a20036aa6bc621107763d6bf569650e5b
DooYoungHo <pimang223@naver.com>
2026-02-18 23:42:43 +0900

    fix: usermapper mapping 수정
    Users -> users로 변경

--- daebbang-api/src/main/java/com/daebbang/daebbangapi/mapper/user/UserMapper.java
+++ daebbang-api/src/main/java/com/daebbang/daebbangapi/mapper/user/UserMapper.java
@@ -8,7 +8,7 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", imports = {PhoneNumberUtils.class})
 public interface UserMapper {
-    @Mapping(source = "Users.name", target = "userName")
+    @Mapping(source = "users.name", target = "userName")
     @Mapping(target = "userPhoneNumber", expression = "java(PhoneNumberUtils.maskPhoneNumber(users.getPhoneNumber()))")
     UserInfo toUserInfo(Users users);
 }
